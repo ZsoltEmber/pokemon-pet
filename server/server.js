97aa28d76171c69e1f3c3b39a1f1397123c45490
ZsoltEmber <132936073+ZsoltEmber@users.noreply.github.com>
2024-03-09 10:17:09 +0100

    install dotenv dependency

---
+++ server/server.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const { MONGO_URL, PORT = 8080 } = process.env;
+
+if (!MONGO_URL) {
+    console.error("Missing MONGO_URL environment variable");
+    process.exit(1);
+  }
+
+
+const app = express()
+app.use(express.json())
+
+
+mongoose.connect()
+.then(()=>{
+    app.listen(3000, () => {
+        console.log('Im in! Open this link: http://127.0.0.1:3000');
+    })
+})
+
+
+
+app.get("/api/pokemon", async(req, res) => {
+    const AllPokemon = await Pokemon.find({})
+    res.send(AllPokemon)
+    res.status(200)
+})
+
+app.get('/api/pokemon/:id', async (req, res) =>{
+    const pokemon = await Pokemon.findById(req.params.id);
+
+    res.json(pokemon)
+})
