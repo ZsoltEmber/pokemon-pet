13499a9e1851a5611fe28442fb93e023e367e220
ZsoltEmber <132936073+ZsoltEmber@users.noreply.github.com>
2024-04-06 23:44:27 +0200

    create foe component

--- client/pokemon-pet/src/components/fight/Foe.jsx
+++ client/pokemon-pet/src/components/fight/Foe.jsx
@@ -1,23 +1,29 @@
-function Foe({pokemon}){
+function Foe({pokemon}) {
 
 
     const foe = {
-        name: name,
+        name: pokemon.name,
         front: pokemon.sprites.versions['generation-v']['black-white'].animated.front_default,
         back: pokemon.sprites.versions['generation-v']['black-white'].animated.back_default,
         hp: pokemon.stats[0].base_stat,
         attack: pokemon.stats[1].base_stat,
         defense: pokemon.stats[2].base_stat,
         xp: 0,
+        height: pokemon.height,
         types: pokemon.types,
     }
 
+    console.log(foe.height)
 
-    return(
+
+    return (
         <div>
-            <div className="pokemonNickname" name={foe._id} >{foe.name.toUpperCase() }
+            <div className="pokemonNickname">
+                {foe.name.toUpperCase()}
             </div>
-            <img className="sprite" name={foe._id} src={foe.front} alt={foe.name}>
+            <img className="fighter-sprite"
+                 src={foe.front}
+                 alt={foe.name}>
             </img>
         </div>
     )
